0ceb9e8320129f285d1b3a20e2ded08c13e3fa3d
SmKou <66178856+SmKou@users.noreply.github.com>
2023-07-01 21:50:38 -0700

    PASS Creature

--- __tests__/Creature.test.js
+++ __tests__/Creature.test.js
@@ -1,5 +1,5 @@
 import each from 'jest-each';
-import Creature from '../src/js/bs/Creature';
+import makeCreature from '../src/js/bs/Creature';
 
 /* Refer testing_guide.md */
 
@@ -8,9 +8,14 @@ describe("Creature", () => {
         [{ x: 1, y: 1, dx: 0, dy: 0 }, 1, 1],
         [{ x: 1, y: 0, dx: 0, dy: 0 }, 1, 0],
         [{ x: 0, y: 1, dx: 0, dy: 0 }, 0, 1],
-        [{ x: 0, y: 0, dx: 0, dy: 0 }, 0, 0]
-    ]).test("Should create a creature %s at %s, %s", (expected, x, y) => {
-        const creature = new Creature({ x, y });
-        expected(creature).toEqual(expected);
+        [{ x: 0, y: 0, dx: 0, dy: 0 }, 0, 0],
+        [{ x: -100, y: 1, dx: 0, dy: 0 }, '1', 1],
+        [{ x: 1, y: -100, dx: 0, dy: 0 }, 1, '1']
+    ]).test("Should create a creature %s at %s, %s", (outcome, x, y) => {
+        const creature = makeCreature({ x, y });
+        expect(creature.x).toEqual(outcome.x);
+        expect(creature.y).toEqual(outcome.y);
+        expect(creature.dx).toEqual(outcome.dx);
+        expect(creature.dy).toEqual(outcome.dy);
     });
 });
--- src/js/bs/Creature.js
+++ src/js/bs/Creature.js
@@ -1,3 +1,27 @@
-export default class Creature {
-    constructor() {}
+class Creature {
+    constructor(params) {
+        const { x, y } = params;
+        this.x = x;
+        this.y = y;
+        this.dx = 0;
+        this.dy = 0;
+    }
+}
+
+const position = {
+    x: -100,
+    y: -100
+}
+
+/**
+ * Receives an x and y coordinate for generating a new creature. If a coordinate is invalid, it is replaced by the default coordinate. Default coordinates cause the creature to be rendered off map.
+ * @param {Object} params 
+ * @param {Number} params.x
+ * @param {Number} params.y
+ * @returns {Creature}
+ */
+export default function makeCreature(params) {
+    params.x = params.hasOwnProperty('x') && typeof params.x === 'number' ? params.x : position.x;
+    params.y = params.hasOwnProperty('y') && typeof params.y === 'number' ? params.y : position.y;
+    return new Creature({ x: params.x, y: params.y });
 }
