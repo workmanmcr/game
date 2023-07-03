5f7aec1ae49b6f399ab45f75de41f48b4b0948bc
workmanmcr <workmanmcr@gmail.com>
2023-07-03 14:40:34 -0700

    add character location, size, health,

---
+++ src/js/bs/Player.js
@@ -0,0 +1,25 @@
+export default class Player {
+  constructor() {
+      // this.width = width;
+      // this.height = height;
+      // this.dy = 0;
+      // this.dx = 1;
+      // this.angle = 0; 
+      // this.speed = 1;
+      // this.x = x;
+      // this.y = y;
+      // this.health = 3;
+  }
+  
+
+    // movement() {
+    //   if (key === 'W' || key === 'w') {
+    //     player.moveUp();
+    //   } else if (key === 'S' || key === 's') {
+    //     player.moveDown();
+    //   } else if (key === 'A' || key === 'a') {
+    //     player.moveLeft();
+    //   } else if (key === 'D' || key === 'd') {
+    //     player.moveRight();
+    //   }
+}
