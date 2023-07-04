f368afb68dfba80d5e24cc9f3e2a9855b46c27cd
SmKou <66178856+SmKou@users.noreply.github.com>
2023-07-04 14:05:41 -0700

    Mod passed tests with renames

--- __tests__/Ammunition.test.js
+++ __tests__/Ammunition.test.js
@@ -1,18 +1,18 @@
 import each from 'jest-each';
-import makeSting from "../src/js/bs/Sting";
+import makeAmmunition from "../src/js/bs/Ammunition";
 
-describe("Sting", () => {
+describe("Ammunition", () => {
     each([
         [{ x: 1, y: 1, angle: 0 }, 1, 1, 0],
         [{ x: 1, y: 1, angle: Math.PI }, 1, 1, Math.PI],
         [{ x: -100, y: 1, angle: 0 }, '1', 1, 0],
         [{ x: 1, y: -100, angle: 0 }, 1, '1', 0],
         [{ x: 1, y: 1, angle: 0 }, 1, 1, '0']
-    ]).test("should create a sting %s with x=%s, y=%s and an angle of %s", (outcome, x, y, angle) => {
-        const sting = makeSting({ x, y, angle });
-        expect(sting.x).toBe(outcome.x);
-        expect(sting.y).toBe(outcome.y);
-        expect(sting.angle).toBeCloseTo(outcome.angle);
+    ]).test("should create a ammunition %s with x=%s, y=%s and an angle of %s", (outcome, x, y, angle) => {
+        const ammunition = makeAmmunition({ x, y, angle });
+        expect(ammunition.x).toBe(outcome.x);
+        expect(ammunition.y).toBe(outcome.y);
+        expect(ammunition.angle).toBeCloseTo(outcome.angle);
     });
 
     const pos = { x: 32, y: 32 };
@@ -28,9 +28,9 @@ describe("Sting", () => {
         [(3 / 4) * Math.PI],
         [Math.PI]
     ]).test("should move x and y by cos and sin of %s * 16", (angle) => {
-        const sting = makeSting({ ...pos, angle });
-        sting.move();
-        expect(sting.x).toBeCloseTo(pos.x + (16 * Math.cos(angle)));
-        expect(sting.y).toBeCloseTo(pos.y + (16 * Math.sin(angle)));
+        const ammunition = makeAmmunition({ ...pos, angle });
+        ammunition.move();
+        expect(ammunition.x).toBeCloseTo(pos.x + (16 * Math.cos(angle)));
+        expect(ammunition.y).toBeCloseTo(pos.y + (16 * Math.sin(angle)));
     });
 });
