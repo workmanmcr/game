95c5b4500be6101b249b534eb4e6250c640fbfc1
Thomas Bakken <tbakken14@gmail.com>
2023-07-05 15:09:02 -0700

    Update Map Generator

--- js/bs/MapGenerator.js
+++ js/bs/MapGenerator.js
@@ -1,17 +1,47 @@
 class MapGenerator {
-    constructor(dimensions, seed) {
-        [this.i, this.j] = dimensions;
+    constructor(dimensions, granularity, tileSize, seed) {
+        this.dimensions = dimensions;
+        this.seed = seed;
+        noiseSeed(seed);
+        this.granularity = granularity;
+        this.tileSize = tileSize;
+        this.generateMap();
     }
 
-    addNoise() {
-        noiseSeed(1);
-        let map = [];
-        for (let i = 0; i < this.i; i++) {
-            map[i] = [];
-            for (let j = 0; j < this.j; j++) {
-                map[i][j] = noise(i, j);
+    generateMap() {
+        this.map = [];
+        for (let i = 0; i < this.dimensions[0]; i++) {
+            this.map[i] = [];
+            for (let j = 0; j < this.dimensions[1]; j++) {
+                this.map[i][j] = noise(i / this.granularity, j / this.granularity);
             }
         }
-        return map;
+    }
+
+    setColor(value) {
+        if (value < .3) {
+            fill('blue');
+        }
+        else if (value < .4) {
+            fill('yellow');
+        }
+        else if (value < .5) {
+            fill('green');
+        }
+        else if (value < .7) {
+            fill('darkgreen');
+        }
+        else {
+            fill('grey');
+        }
+    }
+
+    draw() {
+        for (let i = 0; i < this.dimensions[0]; i++) {
+            for (let j = 0; j < this.dimensions[1]; j++) {
+                this.setColor(this.map[i][j]);
+                rect(i * this.tileSize - width / 2, j * this.tileSize - height / 2, this.tileSize, this.tileSize);
+            }
+        }
     }
 }
