3704f94d12672750ae02590beb1f41c64c6063ee
workmanmcr <workmanmcr@gmail.com>
2023-07-05 10:10:17 -0700

    add shoot buttons and aiming methods

--- src/js/bs/Player.js
+++ src/js/bs/Player.js
@@ -6,15 +6,16 @@ export default class Player {
     this.speed = 2;
     this.pos = { x, y };
     this.health = 3;
-  }
-
+  } 
+  // press A to move left, D to move right, W to move up, S to move down
+  // press J and L to aim and spacebar to shoot.
   move(p) {
     if (p.keyIsDown(65)) {
       this.dx = -1;
     }
     if (p.keyIsDown(83)) {
       this.dy = 1;
-    }
+    } 
     if (p.keyIsDown(87)) {
       this.dy = -1;
     }
@@ -23,12 +24,23 @@ export default class Player {
     }
     this.pos.x += this.dx * this.speed;
   }
+  updateAim(p) {
+    const mouseAngle = Math.atan2(p.mouseY - this.pos.y, p.mouseX - this.pos.x);
+    this.angle = mouseAngle;
+  }
   shoot(p) {
-    if (p.keyIsDown(32)) {
+    if (p.keyIsDown(74)) { 
+      this.angle -= 0.1; 
+    }
+    if (p.keyIsDown(76)) { 
+      this.angle += 0.1; 
+    }
+    if (p.keyIsDown(32, 75) || p.mouseIsPressed) {
       this.stings.push(makeAmmunition({
         x: this.pos.x,
         y: this.pos.y,
         angle: this.angle
       }))
     }
-  };
+  }
+};
