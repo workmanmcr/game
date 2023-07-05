b993c71f0a52f175a3c0c5f5d899fe6d25b467af
workmanmcr <workmanmcr@gmail.com>
2023-07-05 10:26:57 -0700

    remove node and p

--- src/js/ui/Draw.js
+++ src/js/ui/Draw.js
@@ -1,12 +1,10 @@
-import App from '../bs/App';
-
-export function drawPlayer(p, player) {
-    p.fill(255);
-    p.stroke(0);
-    p.push();
-    p.translate(player.pos.x, player.pos.y);
-    p.rotate(player.angle);
-    p.rect(0, 0, App.unit, App.unit);
+function drawPlayer(player) {
+    fill(255);
+    stroke(0);
+    push();
+    translate(player.pos.x, player.pos.y);
+    rotate(player.angle);
+    rect(0, 0, App.unit, App.unit);
 
     for (let bullet of player.bullets) {
         bullet.move();
@@ -15,33 +13,32 @@ export function drawPlayer(p, player) {
 }
 
 
-function drawBullet(p, bullet) {
+function drawBullet(bullet) {
     const size = App.unit / 4;
-    p.fill(0);
-    p.noStroke();
-    p.circle(bullet.x, bullet.y, size);
+    fill(0);
+    noStroke();
+    circle(bullet.x, bullet.y, size);
 }
-
-export function drawCreature(p, creature) {
-    p.fill(creature.color);
-    p.push();
-    p.translate(creature.pos.x, creature.pos.y);
-    p.rotate(creature.angle);
-    p.rect(0, 0, App.unit, App.unit);
-    p.pop();
+function drawCreature(creature) {
+    fill(creature.color);
+    push();
+    translate(creature.pos.x, creature.pos.y);
+    rotate(creature.angle);
+    rect(0, 0, App.unit, App.unit);
+    pop();
 
     for (let sting of creature.stings) {
         sting.move();
-        drawSting(p, sting);
+        drawSting(sting);
     }
 }
 
-function drawSting(p, sting) {
+function drawSting(sting) {
     const size = App.unit / 4;
-    p.noFill();
-    p.stroke(0);
-    p.strokeWeight(3);
+    noFill();
+    stroke(0);
+    strokeWeight(3);
     const xHalf = size * Math.cos(sting.angle);
     const yHalf = size * Math.sin(sting.angle);
-    p.line(sting.x + xHalf, sting.y + yHalf, sting.x - xHalf, sting.y - yHalf);
+    line(sting.x + xHalf, sting.y + yHalf, sting.x - xHalf, sting.y - yHalf);
 }
