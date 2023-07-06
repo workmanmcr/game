697c629ce285bf5fa0eef2f7ffce18d6685e8f44
SmKou <66178856+SmKou@users.noreply.github.com>
2023-07-05 19:37:58 -0700

    WIP Add player bounds

--- js/bs/Player.js
+++ js/bs/Player.js
@@ -1,70 +1,75 @@
 class Player {
-  constructor(x, y) {
-    this.speed = 2;
-    this.angle = 0;
-    this.pos = createVector(x, y);
-    this.bullets = [];
-    this.health = app.max_health;
-    this.life = 1;
-  } 
-  // press A to move left, D to move right, W to move up, S to move down
-  move() {
-    if (keyIsDown(65)) {
-        this.pos.x += -1 * this.speed;
+    constructor(x, y) {
+        this.speed = 5;
+        this.angle = 0;
+        this.pos = createVector(x, y);
+        this.bullets = [];
+        this.health = app.max_health;
+        this.life = 1;
     }
-    if (keyIsDown(83)) {
-        this.pos.y += 1 * this.speed;
-    }
-    if (keyIsDown(87)) {
-        this.pos.y += -1 * this.speed;
-    }
-    if (keyIsDown(68)) {
-        this.pos.x += 1 * this.speed;
-    }
-    if (keyIsDown(74)) {
-        this.angle -= 0.1;
-    }
-    if (keyIsDown(76)) {
-        this.angle += 0.1;
+    // press A to move left, D to move right, W to move up, S to move down
+    move() {
+        if (keyIsDown(65)) {
+            this.pos.x += -1 * this.speed;
+        }
+        if (keyIsDown(83)) {
+            this.pos.y += 1 * this.speed;
+        }
+        if (keyIsDown(87)) {
+            this.pos.y += -1 * this.speed;
+        }
+        if (keyIsDown(68)) {
+            this.pos.x += 1 * this.speed;
+        }
+        if (keyIsDown(74)) {
+            this.angle -= 0.1;
+        }
+        if (keyIsDown(76)) {
+            this.angle += 0.1;
+        }
+
+        if (keyIsDown(75) || mouseIsPressed) {
+            this.shoot();
+        }
+
+        for (const bullet of this.bullets)
+            bullet.move();
     }
 
-    if (keyIsDown(75) || mouseIsPressed) {
-        this.shoot();
+    moveInBounds(pos, d, lowerBound, higherBound) {
+        if (pos + d * this.speed > lowerBound && pos + d * this.speed < higherBound)
     }
 
-    for (const bullet of this.bullets)
-        bullet.move();
-  }
-  updateAim() {
-    const mouseAngle = Math.atan2(mouseY - this.pos.y, mouseX - this.pos.x);
-    this.angle = mouseAngle;
-  }
-  // press J and L to aim and spacebar to shoot or use mouse to aim and left click to shoot.
-  shoot() {
-    this.bullets.push(makeAmmunition({
-        x: this.pos.x,
-        y: this.pos.y,
-        angle: this.angle,
-        type: 'bullet'
-      }))
+    updateAim() {
+        const mouseAngle = Math.atan2(mouseY - this.pos.y, mouseX - this.pos.x);
+        this.angle = mouseAngle;
+    }
+    // press J and L to aim and spacebar to shoot or use mouse to aim and left click to shoot.
+    shoot() {
+        this.bullets.push(makeAmmunition({
+            x: this.pos.x,
+            y: this.pos.y,
+            angle: this.angle,
+            type: 'bullet'
+        }))
     }
 
-  
-hit() {
-    this.health--;
-    return this.health === 0;
-}
 
-  draw() {
-      fill(255);
-      stroke(0);
-      push();
-      translate(this.pos.x, this.pos.y);
-      rotate(this.angle);
-      square(0, 0, app.unit);
-      pop();
+    hit() {
+        this.health--;
+        return this.health === 0;
+    }
 
-      for (const bullet of this.bullets)
-          bullet.draw();
-  }
+    draw() {
+        fill(255);
+        stroke(0);
+        push();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.angle);
+        square(0, 0, app.unit);
+        pop();
+
+        for (const bullet of this.bullets)
+            bullet.draw();
+    }
 };
